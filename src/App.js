0c3e2f29dcc8193c6e0a4a3a01363d22ab9ff6c7
anhduc97hn <anhduc.97hn@gmail.com>
2023-06-11 15:55:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Header />
+    <div className="app">
+      <Container>
+        <Routes>
+        <Route path="/" element={<Trending />} exact />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Container>
     </div>
-  );
+    <SimpleBottomNavigation />
+  </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/MainNav/MainNav.js
@@ -0,0 +1,65 @@
+import React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import TvIcon from "@mui/icons-material/Tv";
+import MovieIcon from "@mui/icons-material/Movie";
+import SearchIcon from "@mui/icons-material/Search";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (value === 0) {
+navigate("/")
+    }
+    else if (value === 1) {
+      navigate("/movies")
+    }  
+    else if (value === 2) {
+      navigate("/series")
+    }
+    else if (value === 3) {
+      navigate("/search")
+    }    
+  }, [value])
+  
+
+  return (
+    <Box sx={{ width: "100%", position: "fixed",  bottom: 0, zIndex: 100  }}>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      style={{backgroundColor: "#2d313a"}}
+    >
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Trending"
+        icon={<WhatshotIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TvIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
+    </BottomNavigation>
+     </Box>
+  );
+}
